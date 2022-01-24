9f32c4744d66cf97547996268f67aab54dbecdb1
Sulayman Matkuliyev <suleymantm11@gmail.com>
2022-01-24 15:02:38 +0300

    son

--- src/test/java/org/junit/J06_TestingPerformance.java
+++ src/test/java/org/junit/J06_TestingPerformance.java
@@ -15,6 +15,6 @@ public class J06_TestingPerformance {
      //   assertTimeout(Duration.ofSeconds(2),() -> IntStream.rangeClosed(0,100).sum());
      //   assertTimeout(Duration.ofSeconds(2),() -> IntStream.rangeClosed(0,100000).forEach(System.out::print));    // print performansa dahil
      //   assertTimeout(Duration.ofSeconds(1),() -> IntStream.rangeClosed(0,100000)).forEach(System.out::print);      // print performansa dahil degil
-        assertTimeout(Duration.ofNanos(1),() -> LongStream.rangeClosed(0,100000)).forEach(System.out::print);
+        assertTimeout(Duration.ofNanos(10),() -> LongStream.rangeClosed(0,100000)).forEach(System.out::print);
     }
 }
